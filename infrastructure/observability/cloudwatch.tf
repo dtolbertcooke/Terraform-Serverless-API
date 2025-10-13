dd585bf8c2d7c9335998e46e3a17d19410f47186
Doug T. <dtolbertcooke@gmail.com>
2025-10-13 16:09:09 -0400

    terraform fmt

--- infrastructure/observability/cloudwatch.tf
+++ infrastructure/observability/cloudwatch.tf
@@ -50,7 +50,7 @@ resource "aws_api_gateway_method_settings" "path_specific" {
 # Log groups for lambda functions
 resource "aws_cloudwatch_log_group" "lambda_logs" {
   for_each = local.config.api_methods
-  
+
   name              = "/aws/lambda/fruit-api-${each.value}"
   retention_in_days = 14
 
