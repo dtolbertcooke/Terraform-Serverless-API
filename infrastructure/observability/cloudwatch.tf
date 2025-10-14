82239b5c3adbd79ac8d7192189e8e42c182a958a
Doug T. <dtolbertcooke@gmail.com>
2025-10-14 12:44:36 -0400

    fixing execution logs

--- infrastructure/observability/cloudwatch.tf
+++ infrastructure/observability/cloudwatch.tf
@@ -3,12 +3,32 @@ resource "aws_api_gateway_account" "api_gateway_role" {
   cloudwatch_role_arn = var.api_gateway_role_arn
 }
 
-# create access logging for api
-# creates the CloudWatch log group that receives API Gateway access logs
-resource "aws_cloudwatch_log_group" "api_logs" {
+# create access logs group for api
+resource "aws_cloudwatch_log_group" "api_access_logs" {
   name              = "/apigw/fruits-api-access-logs-${var.environment}"
-  retention_in_days = 14 # make configurable via a variable (7 for dev, 30 for prod)
+  retention_in_days = var.environment == "prod" ? 14 : 7 # longer retention for prod
+
+  tags = {
+    Environment = var.environment
+    Project     = var.project_name
+    Owner       = var.owner
+    ManagedBy   = "Terraform"
+  }
 }
+
+# create execution logs group for api
+resource "aws_cloudwatch_log_group" "api_execution_logs" {
+  name              = "/apigw/${var.project_name}-execution-logs-${var.environment}"
+  retention_in_days = var.environment == "prod" ? 14 : 7 # longer retention for prod
+
+  tags = {
+    Environment = var.environment
+    Project     = var.project_name
+    Owner       = var.owner
+    ManagedBy   = "Terraform"
+  }
+}
+
 # create api gateway stage
 # defines the access log format and destination for api 
 resource "aws_api_gateway_stage" "api_stage" {
@@ -16,8 +36,9 @@ resource "aws_api_gateway_stage" "api_stage" {
   rest_api_id   = var.rest_api_id
   deployment_id = var.rest_api_deployment_id
 
+  # send access logs to cloudwatch
   access_log_settings {
-    destination_arn = aws_cloudwatch_log_group.api_logs.arn
+    destination_arn = aws_cloudwatch_log_group.api_access_logs.arn
     format = jsonencode({
       requestId    = "$context.requestId",
       ip           = "$context.identity.sourceIp",
@@ -30,20 +51,21 @@ resource "aws_api_gateway_stage" "api_stage" {
     })
   }
   # visualize request traces in AWS X-Ray
-  xray_tracing_enabled = true # make configurable via a variable (true for prod, false for dev & test)
+  xray_tracing_enabled = var.environment == "prod" ? true : false
   depends_on           = [aws_api_gateway_account.api_gateway_role]
 }
 
-# Full Request and Response Logs
+# Method-Level Logging and Metrics
 resource "aws_api_gateway_method_settings" "path_specific" {
   rest_api_id = var.rest_api_id
   stage_name  = aws_api_gateway_stage.api_stage.stage_name
   method_path = "*/*"
 
   settings {
-    logging_level      = "INFO" # enables detailed execution logging
-    metrics_enabled    = true   # publishes API Gateway metrics to CloudWatch
-    data_trace_enabled = true   # logs full request/response payloads (useful in dev/test, disable in prod for cost/security)
+    logging_level      = "INFO" # enables execution logging
+    # logging_level      = var.environment == "prod" ? "ERROR" : "INFO"
+    metrics_enabled    = true # publishes API gateway metrics to cloudwatch
+    data_trace_enabled = var.environment == "prod" ? false : true # logs full request/response payloads (useful in dev/test, disable in prod for cost/security)
   }
 }
 
@@ -52,7 +74,7 @@ resource "aws_cloudwatch_log_group" "lambda_logs" {
   for_each = var.api_methods
 
   name              = "/aws/lambda/fruit-api-${each.value}-${var.environment}"
-  retention_in_days = 14
+  retention_in_days = var.environment == "prod" ? 30 : 14 # longer retention for prod
 
   tags = {
     Environment = var.environment
