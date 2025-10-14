ca1e91239bcf4d394fcad9f847d2020b81ff3ee3
Doug T. <dtolbertcooke@gmail.com>
2025-10-14 12:49:04 -0400

    validation

--- infrastructure/observability/outputs.tf
+++ infrastructure/observability/outputs.tf
@@ -1,3 +1,3 @@
-output "api_gateway_log_group_arn" {
-  value = aws_cloudwatch_log_group.api_logs.arn # update this
+output "api_gateway_access_log_group_arn" {
+  value = aws_cloudwatch_log_group.api_access_logs.arn
 }
